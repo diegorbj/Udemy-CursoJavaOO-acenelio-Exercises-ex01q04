b8a5bfbda2a1b49eab87dc2b70bd218f228f327d
Diego Barros <diegorbj@outlook.com>
2021-03-15 14:13:47 -0300

    Versão inicial.

---
+++ src/aplication/Main.java
@@ -0,0 +1,23 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        int funcionario = sc.nextInt();
+        int horasTrabalhadas = sc.nextInt();
+        double valorHora = sc.nextDouble();
+        double salario = horasTrabalhadas * valorHora;
+
+        System.out.println("NUMBER: " + funcionario);
+        System.out.printf("SALARY: U$ %.2f%n", salario);
+
+        sc.close();
+    }
+}
